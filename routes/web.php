7473ceecf20ec637d8c9e4c4f6ec3498b97c2311
Jefryanus Lemur <jefrylemur@gmail.com>
2026-02-28 21:39:49 +0700

    test: add test route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('landing.index');
+Route::get('/test', function () {
+    return 'Laravel OK - ' . config('app.env');
 });
